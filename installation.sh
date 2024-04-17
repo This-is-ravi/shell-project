eff75f3fc3052da8988c85987eb99524070a7f13
This-is-ravi <ravivenkat508@gmail.com>
2024-04-17 11:19:29 +0530

    hello script

--- installation.sh
+++ installation.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-USERID = $(ID -U)
-if [$USERID -ne 0]
-then 
-   echo"please run the this script with root user"
+
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql...FAILURE"
+    exit 1
+else
+    echo "Installation of mysql...SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo "Installation of Git...SUCCESS"
+fi
+
+echo "is script proceeding?"
